0c829d7576fb6211cec5e96112d59e552088ae53
Mohamed Adel <adelm15@icloud.com>
2026-01-18 02:50:24 +0200

    fix doctype and html structure

--- flavorspot.js
+++ flavorspot.js
@@ -1,123 +1,123 @@
- document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
 
     const menuData = {
-    pasta: [
-        {
-            name: "Spaghetti Carbonara",
-            desc: "Creamy pasta with bacon and eggs.",
-            price: "120 EGP",
-            img: "img/SpaghettiCarbonara.jpg"
-        },
-        {
-            name: "Penne Arrabbiata",
-            desc: "Spicy tomato sauce with penne.",
-            price: "100 EGP",
-            img: "img/PenneArrabbiata.jpg"
-        },
-        {
-            name: "Fettuccine Alfredo",
-            desc: "Rich cream sauce with chicken.",
-            price: "135 EGP",
-            img: "img/FettuccineAlfredo.jpg"
-        },
-        {
-            name: "Lasagna",
-            desc: "Layers of pasta, meat and cheese.",
-            price: "150 EGP",
-            img: "img/Lasagna.jpg"
-        },
-        {
-            name: "Pesto Pasta",
-            desc: "Basil pesto with cherry tomatoes.",
-            price: "110 EGP",
-            img: "img/PestoPasta.jpg"
-        },
-        {
-            name: "Mac & Cheese",
-            desc: "Cheesy comfort pasta.",
-            price: "95 EGP",
-            img: "img/Mac&Cheese111.jpg"
-        }
-    ],
+        pasta: [
+            {
+                name: "Spaghetti Carbonara",
+                desc: "Creamy pasta with bacon and eggs.",
+                price: "120 EGP",
+                img: "img/SpaghettiCarbonara.jpg"
+            },
+            {
+                name: "Penne Arrabbiata",
+                desc: "Spicy tomato sauce with penne.",
+                price: "100 EGP",
+                img: "img/PenneArrabbiata.jpg"
+            },
+            {
+                name: "Fettuccine Alfredo",
+                desc: "Rich cream sauce with chicken.",
+                price: "135 EGP",
+                img: "img/FettuccineAlfredo.jpg"
+            },
+            {
+                name: "Lasagna",
+                desc: "Layers of pasta, meat and cheese.",
+                price: "150 EGP",
+                img: "img/Lasagna.jpg"
+            },
+            {
+                name: "Pesto Pasta",
+                desc: "Basil pesto with cherry tomatoes.",
+                price: "110 EGP",
+                img: "img/PestoPasta.jpg"
+            },
+            {
+                name: "Mac & Cheese",
+                desc: "Cheesy comfort pasta.",
+                price: "95 EGP",
+                img: "img/Mac&Cheese111.jpg"
+            }
+        ],
 
-    fries: [
-        {
-            name: "Classic Fries",
-            desc: "Golden crispy fries.",
-            price: "60 EGP",
-            img: "img/ClassicFries.jpg"
-        },
-        {
-            name: "Cheese Fries",
-            desc: "Fries topped with melted cheese.",
-            price: "80 EGP",
-            img: "img/CheeseFries.jpg"
-        },
-        {
-            name: "Loaded Fries",
-            desc: "Cheese, beef & sauce.",
-            price: "95 EGP",
-            img: "img/LoadedFries.jpg"
-        },
-        {
-            name: "Garlic Fries",
-            desc: "Fries with garlic butter.",
-            price: "70 EGP",
-            img: "img/GarlicFries.jpg"
-        },
-        {
-            name: "Spicy Fries",
-            desc: "Fries with spicy seasoning.",
-            price: "65 EGP",
-            img: "img/SpicyFries.jpg"
-        },
-        {
-            name: "Truffle Fries",
-            desc: "Premium truffle flavor.",
-            price: "110 EGP",
-            img: "img/TruffleFries.jpg"
-        }
-    ],
+        fries: [
+            {
+                name: "Classic Fries",
+                desc: "Golden crispy fries.",
+                price: "60 EGP",
+                img: "img/ClassicFries.jpg"
+            },
+            {
+                name: "Cheese Fries",
+                desc: "Fries topped with melted cheese.",
+                price: "80 EGP",
+                img: "img/CheeseFries.jpg"
+            },
+            {
+                name: "Loaded Fries",
+                desc: "Cheese, beef & sauce.",
+                price: "95 EGP",
+                img: "img/LoadedFries.jpg"
+            },
+            {
+                name: "Garlic Fries",
+                desc: "Fries with garlic butter.",
+                price: "70 EGP",
+                img: "img/GarlicFries.jpg"
+            },
+            {
+                name: "Spicy Fries",
+                desc: "Fries with spicy seasoning.",
+                price: "65 EGP",
+                img: "img/SpicyFries.jpg"
+            },
+            {
+                name: "Truffle Fries",
+                desc: "Premium truffle flavor.",
+                price: "110 EGP",
+                img: "img/TruffleFries.jpg"
+            }
+        ],
 
-    desserts: [
-        {
-            name: "Chocolate Cake",
-            desc: "Rich chocolate cake.",
-            price: "85 EGP",
-            img: "img/ChocolateCake.jpg"
-        },
-        {
-            name: "Cheesecake",
-            desc: "Creamy cheesecake with sauce.",
-            price: "80 EGP",
-            img: "img/Creamycheesecake.jpg"
-        },
-        {
-            name: "Brownie",
-            desc: "Fudgy chocolate brownie.",
-            price: "60 EGP",
-            img: "img/Brownie.jpg"
-        },
-        {
-            name: "Tiramisu",
-            desc: "Classic Italian dessert.",
-            price: "90 EGP",
-            img: "img/Tiramisu.jpg"
-        },
-        {
-            name: "Ice Cream",
-            desc: "Vanilla ice cream scoop.",
-            price: "45 EGP",
-            img: "img/IceCream.jpg"
-        },
-        {
-            name: "Panna Cotta",
-            desc: "Soft creamy dessert.",
-            price: "85 EGP",
-            img: "img/PannaCotta.jpg"
-        }
-    ]
-};
+        desserts: [
+            {
+                name: "Chocolate Cake",
+                desc: "Rich chocolate cake.",
+                price: "85 EGP",
+                img: "img/ChocolateCake.jpg"
+            },
+            {
+                name: "Cheesecake",
+                desc: "Creamy cheesecake with sauce.",
+                price: "80 EGP",
+                img: "img/Creamycheesecake.jpg"
+            },
+            {
+                name: "Brownie",
+                desc: "Fudgy chocolate brownie.",
+                price: "60 EGP",
+                img: "img/Brownie.jpg"
+            },
+            {
+                name: "Tiramisu",
+                desc: "Classic Italian dessert.",
+                price: "90 EGP",
+                img: "img/Tiramisu.jpg"
+            },
+            {
+                name: "Ice Cream",
+                desc: "Vanilla ice cream scoop.",
+                price: "45 EGP",
+                img: "img/IceCream.jpg"
+            },
+            {
+                name: "Panna Cotta",
+                desc: "Soft creamy dessert.",
+                price: "85 EGP",
+                img: "img/PannaCotta.jpg"
+            }
+        ]
+    };
 
     const loadingScreen = document.getElementById("loading-screen");
     const mainMenu = document.getElementById("main-menu");
@@ -125,15 +125,15 @@
     const menuGrid = document.querySelector(".menu-grid");
 
     function renderMenu(category) {
-    menuGrid.classList.add("fade-out");
+        menuGrid.classList.add("fade-out");
 
-    setTimeout(() => {
-        menuGrid.innerHTML = "";
+        setTimeout(() => {
+            menuGrid.innerHTML = "";
 
-        menuData[category].forEach(item => {
-            const div = document.createElement("div");
-            div.className = "menu-item";
-            div.innerHTML = `
+            menuData[category].forEach(item => {
+                const div = document.createElement("div");
+                div.className = "menu-item";
+                div.innerHTML = `
                 <img src="${item.img}" loading="lazy">
                 <div class="menu-item-content">
                     <h3>${item.name}</h3>
@@ -141,12 +141,12 @@
                     <span class="price">${item.price}</span>
                 </div>
             `;
-            menuGrid.appendChild(div);
-        });
+                menuGrid.appendChild(div);
+            });
 
-        menuGrid.classList.remove("fade-out");
-    }, 300);
-}
+            menuGrid.classList.remove("fade-out");
+        }, 300);
+    }
 
 
     tabs.forEach(tab => {
